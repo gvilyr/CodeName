758607667274e3cdab4de3c81a63a0bbe1ec21df
gvilyr <gvilyr@post.bgu.ac.il>
2020-08-16 14:47:55 +0300

    Adding basic pages complete

---
+++ Client/codenameclient/src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './App.css';
+import Welcome from './components/Welcome'
+import Login from './components/Login'
+import Register from './components/Register'
+import StartGame from './components/StartGame'
+import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom'
+
+//font-family: 'Julius Sans One', sans-serif;
+function App() {
+  return (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Welcome}/>
+            <Route exact path="/Login" component={Login}/>
+            <Route exact path="/Register" component={Register}/>
+            <Route exact path="/StartGame" component={StartGame}/>
+        </Switch>
+    </Router>
+  );
+}
+
+export default App;
